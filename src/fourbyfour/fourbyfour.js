2a491325f315a9e14bf51ee7c4915adff760e0c8
Mike Puangmalai <mike@nongaap.com>
2017-02-16 23:43:56 -0800

    four by four board game and test file added

---
+++ src/fourbyfour/fourbyfour.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const game = require('../game/game');
+
+const winningCombinations = [
+		[0, 1, 2, 3],
+		[4, 5, 6, 7],
+		[8, 9, 10, 11],
+		[12, 13, 14, 15],
+		[0, 4, 8, 12],
+		[1, 5, 9, 13],
+		[2, 6, 10, 14],
+		[3, 7, 11, 15],
+		[0, 5, 10, 15],
+		[3, 6, 9, 12]
+	];
+
+function currentTurn(board) {
+	let x = 0;
+	let o = 0;
+
+	board.forEach(element => {
+		if(element === 'X') x++
+		if(element === 'O') o++
+	});
+
+	return x === o ? 'X' : 'O'
+}
+
+function gameProgress(board) {
+	let count = 0;
+
+	board.forEach(element => {
+		if(element !== '') count++
+	});
+
+	return count
+}
+
+function fourbyfourAI(board, maxDepth = 7){
+	let openMarker = '';
+	if(board.indexOf('') === -1) return board;
+
+	if(game.winningCombo(board, winningCombinations,['X','O'])) return board;
+
+	let computerMarker = currentTurn(board)
+	let opponentMarker = computerMarker !== 'X' ? 'X' : 'O'
+
+	if(gameProgress(board) < 8) return game.gameManager(board, winningCombinations,computerMarker,opponentMarker, openMarker, game.aiManager(game.minimax, [0, 3, -Infinity, Infinity]));
+
+	return game.gameManager(board, winningCombinations,computerMarker,opponentMarker, openMarker, game.aiManager(game.minimax, [0, maxDepth, -Infinity, Infinity]));
+}
+
+console.log(fourbyfourAI(['X','X','X','O','X','O','O','','','X','','','O','O','X','O']))
+
+module.exports = {winningCombinations, currentTurn, gameProgress, fourbyfourAI}
