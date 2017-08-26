9a00a37f20dd70404adb8bac478abdda63d48852
Mike Puangmalai <mike@nongaap.com>
2017-08-25 22:53:44 -0700

    Webpack, Package.json, babelrc, and eslint updated to support React frontend

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = ['react', 'react-dom'];
+
+module.exports = {
+  entry: {
+    bundle: './frontendsrc/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /(\.css|\.scss)$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'frontendsrc/index.html'
+    })
+  ]
+};
